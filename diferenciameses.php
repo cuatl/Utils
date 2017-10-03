cc019f527ea46879483bc2ea2404ed5858dca61c
Jorge Martínez Mauricio <jorgem@gmail.com>
2017-10-03 18:00:32 -0500

    se añadió el cálculo absoluto

--- diferenciameses.php
+++ diferenciameses.php
@@ -1,20 +1,32 @@
 <?php
-   /* 
-   * calcula meses absolutos ??? entre dos fechas 
-   * @toro 2017 https://tar.mx/
-   */
-   //fechas random (inicio, fin)
-   for($i=1;$i<=20;$i++) {
-      $f1 = date('Y-m-d',strtotime('-'.rand(0,30).' month'));
-      $f2 = date('Y-m-d',strtotime('+'.rand(0,3).' month'));
-      printf("meses: %2d ini: %s fin: %s\n",mesesd($f1,$f2),$f1,$f2);
+   /* número de meses entre dos fechas @ToRo https://tar.mx */
+   $fechas = [];
+   //generamos algunas fechas al azar
+   for($i=1;$i<=3;$i++) {
+      $fechas[] = [
+      date('Y-m-d',strtotime('-'.rand(0,10).' month -'.rand(1,30).' day')),
+      date('Y-m-d',strtotime('+'.rand(0,3).' month + '.rand(1,30).' day'))
+      ];
+   }
+   echo "Diferencia absoluta:\n"; //diferencia absoluta (total de meses calculados)
+   foreach($fechas AS $k) {
+      $f1= new DateTime($k[0]);
+      $f2= new DateTime($k[1]);
+      $meses = $f2->diff($f1);
+      $diff =  ($meses->y>0)?$meses->y*12:0;
+      printf("%s a %s -- %s meses %d días\n",$k[0], $k[1], $meses->m+$diff, $meses->d);
+   }
+   echo "\nDiferencia relativa:\n"; //diferencia relativa (meses "pasados")
+   foreach($fechas AS $k) {
+      printf("%s a %s -- %s meses\n",$k[0],$k[1],mesesd($k[0], $k[1]));
    }
    /* mesesd(fecha inicial, fecha final) {{{ */
    function mesesd($f1, $f2) {
       $ff1=strtotime($f1); $ff2=strtotime($f2);
       if(date('Ym',$ff1) == date('Ym',$ff2)) return 0; //mismo mes
       elseif(date('Y',$ff1) == date('Y',$ff2)) { return date('n',$ff2) - date('n',$ff1); } //mismo año
-      else { //otros años
+      else {
+         //otros años
          $anos=[];
          for($i=date('Y',$ff1);$i<=date('Y',$ff2);$i++) { $anos[$i]=$i; }
          $fa = array_shift($anos);//año inicial
